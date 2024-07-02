8f61d896ff9436e4894c0751b114a82d1660052d
MaciejGretkowski <95485882+MaciejGretkowski@users.noreply.github.com>
2024-07-02 13:02:23 +0100

    ADD: function to play a round of the game

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ let computerChoice;
 let userChoice;
 let computerScore = 0;
 let userScore = 0;
-
+let gameContinue;
 
 function getUserChoice() {
     userChoice = prompt("what is your choice?");
@@ -59,5 +59,19 @@ function determineWinner(userChoice, computerChoice) {
     }
 }
 
+function gameLogic() {
+    gameContinue = prompt("Do you want to play the game? /nType in 'Y' for Yes or 'N' for No");
+    if (gameContinue === "N") {
+        return(`Computer score is: ${computerScore}, your score is: ${userScore}`);
+    } else {
+        getComputerChoice();
+        getUserChoice();
+        return determineWinner(userChoice, computerChoice);
+    }
+}
+
 // Quick testing the logic
 // console.log(determineWinner("S", "R") + " " + computerScore + " " + userScore);
+for (let i = 0; i <= 4; i++) {
+    gameLogic();
+}
